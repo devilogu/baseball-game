e897d7b27e14d7d8c13f2e888207ae15afc2ae9b
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2019-12-21 23:48:57 +0900

    Add repeat function

--- src/main/BaseballGame.java
+++ src/main/BaseballGame.java
@@ -3,65 +3,78 @@ package main;
 import java.util.ArrayList;
 
 public class BaseballGame {
-	
+
 	private static final int INPUT_SIZE = 3;
-	
+
 	private Computer computer;
 	private User user;
 	
+	private boolean start = true;
+
 	private ArrayList<Integer> computerNum;
 	private ArrayList<Integer> userNum;
-	
-	private int strike = 0;
-	private int ball = 0;
-	
+
 	public BaseballGame() {
 		computer = new Computer();
 		user = new User();
-		
-	}
-	
-	/* 사용자에게 수를 입력받고, 컴퓨터는 난수 생성 */
-	public void run() {	
-		user.inputNumber();
-		computer.createRandomNum();
-		
-		compare();
 	}
 
-	
-	/* 숫자 비교  */
-	public void compare() {
-		userNum = user.getNum();
-		computerNum = computer.getRandomNum();
-		System.out.println(computerNum);
-		checkStrike();
-		checkball();
+	/* 사용자에게 수를 입력받고, 컴퓨터는 난수 생성 */
+	public void run() {
+		computer.createRandomNum();
 		
-		if(strike <= 0 && ball <= 0) {
+		while (start) {
+			user.inputNumber();
+			compare();
+		}
+	}
+
+	/* 숫자 비교하여 스트라이크, 볼 체크 */
+	public void compare() {
+		int strike = 0;
+		int ball = 0;
+
+		computerNum = computer.getRandomNum();
+		userNum = user.getNum();
+
+		/* 스트라이크 개수 체크 */
+		for (int i = 0; i < INPUT_SIZE; i++) {
+			if (computerNum.get(i).equals(userNum.get(i))) {
+				strike += 1;
+			}
+		}
+		
+		/* 볼 개수 체크 */
+		for (int i = 0; i < INPUT_SIZE; i++) {
+			if (computerNum.contains(userNum.get(i)) && computerNum.get(i) != userNum.get(i)) {
+				ball += 1;
+			}
+		}
+
+		/* 결과 출력  */
+		result(strike, ball);
+		
+		/* 정답일 경우 유저에게 재시작 여부 묻기 */
+		if(strike == 3) {
+			restart();
+		}
+	}
+
+	/* 결과 출력 */
+	public void result(int strike, int ball) {
+		if (strike <= 0 && ball <= 0) {
 			System.out.println("Nothing");
-		}else {
+		} else {
 			System.out.println("strike: " + strike + " ball: " + ball);
 		}
 	}
 	
-	/* 스트라이크 개수 반환  */
-	public void checkStrike() {
-		for(int i=0; i<INPUT_SIZE; i++) {
-			if(computerNum.get(i).equals(userNum.get(i))) {
-				strike += 1;
-			}
+	public void restart() {
+		if(user.restart()) {
+			start = true;
+			computer.createRandomNum();
+		}else {
+			start = false;
 		}
 	}
-	
-	/* 볼 개수 반환 */
-	public void checkball() {
-		for(int i=0; i<INPUT_SIZE; i++) {
-			if(computerNum.contains(userNum.get(i)) && computerNum.get(i) != userNum.get(i)){
-				ball += 1;
-			}
-		}
-	}
-	
-	/* 재시작 여부 */
 }
